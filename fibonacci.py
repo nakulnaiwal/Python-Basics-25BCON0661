86baa19ef804f13ce922389cde195145ba2b3b0a
Nakul Naiwal <nakulnaiwal16@gmail.com>
2026-09-12 09:03:53 +0530

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -7,11 +7,13 @@ Original file is located at
     https://colab.research.google.com/drive/1qnxuxw_ZbdIimcX_6LphqAbWGf7TS3wC
 """
 
-n = int(input("Enter the number of terms: "))
+n = int(input("Enter position: "))
 
 a = 0
 b = 1
 
 for i in range(n):
-    print(a, end=" ")
     a, b = b, a + b
+
+print(a)
+
